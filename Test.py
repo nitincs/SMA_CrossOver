a98119c74bca38586ebddcee9dd33621238695e4
oo <anoophky1@gmail.com>
2019-12-17 12:26:18 +0530

    plot

--- Test.py
+++ Test.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 df = pd.read_csv("Data/1.csv", names=['ind', 'open', 'high', 'low', 'close', 'volume'])
 
-plt.plot(df["ind"], df["close"])
+M20 = df["close"].rolling(20).mean()
+M10 = df["close"].rolling(10).mean()
+
+plt.plot(df["ind"][-100:], df["close"][-100:], label="close")
+plt.plot(df["ind"][-100:], M20[-100:], label="M20")
+plt.plot(df["ind"][-100:], M10[-100:], label="M10")
+
 plt.legend()
 plt.show()
