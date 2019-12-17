cc2209dc562cb9584ec5236099a03bdda6b4db98
oo <anoophky1@gmail.com>
2019-12-17 12:10:00 +0530

    plot

--- Test.py
+++ Test.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 
+import pandas as pd
 
+df = pd.read_csv("Data/1.csv", names=['ind', 'open', 'high', 'low', 'close', 'volume'])
 
-
-
-
-# plt.plot()
-# plt.legend()
-# plt.show()
+plt.plot(df["ind"], df["close"])
+plt.legend()
+plt.show()
