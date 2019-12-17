4cfce451f13a2956ce08f49f12cfa651fb268109
oo <anoophky1@gmail.com>
2019-12-17 15:41:30 +0530

    plot

--- Test.py
+++ Test.py
@@ -4,46 +4,45 @@ import numpy as np
 
 import pandas as pd
 
-df = pd.read_csv("Data/1.csv", names=['ind', 'open', 'high', 'low', 'close', 'volume'])
-
-df["close"] = (df["close"] - df["close"].min()) / (df["close"].max() - df["close"].min())
-M20 = df["close"].rolling(20).mean()
-M10 = df["close"].rolling(10).mean()
-
-
 ar = []
 
+for i in range(1, 50):
+    df = pd.read_csv("Data/" + str(i) + ".csv", names=['ind', 'open', 'high', 'low', 'close', 'volume'])
 
-for i in range(30, len(M20) - 3):
+    df["close"] = (df["close"] - df["close"].min()) / (df["close"].max() - df["close"].min())
+    M20 = df["close"].rolling(20).mean()
+    M10 = df["close"].rolling(10).mean()
 
-    p1 = M20[i] - M10[i]
-    p2 = M20[i - 1] - M10[i - 1]
-    p3 = M20[i - 2] - M10[i - 2]
+    for i in range(30, len(M20) - 3):
 
-    if p1 < 0 < p3 and p2 >= 0:
-        cp = df["close"][i]
+        p1 = M20[i] - M10[i]
+        p2 = M20[i - 1] - M10[i - 1]
+        p3 = M20[i - 2] - M10[i - 2]
 
-        c11 = df["close"][i + 1]
-        c21 = df["close"][i + 2]
-        c31 = df["close"][i + 3]
+        if p1 < 0 < p3 and p2 >= 0:
+            cp = df["close"][i]
 
-        c1 = (c11 - cp) / cp
-        c2 = (c21 - cp) / cp
-        c3 = (c31 - cp) / cp
-        print(c1, c2, c3)
-        pc = (c1 * c11 + c2 * c21 + c3 * c31) / (abs(c11) + abs(c2) + abs(c3))
+            c11 = df["close"][i + 1]
+            c21 = df["close"][i + 2]
+            c31 = df["close"][i + 3]
 
-        print(df["close"][i - 9:i + 4])
-        print(cp, pc * 100)
-        ar.append(pc*100)
+            c1 = (c11 - cp) / cp
+            c2 = (c21 - cp) / cp
+            c3 = (c31 - cp) / cp
+            # print(c1, c2, c3)
+            pc = (c1 * c11 + c2 * c21 + c3 * c31) / (abs(c1) + abs(c2) + abs(c3))
+
+            # print(df["close"][i - 9:i + 4])
+            # print(cp, pc * 100)
+            ar.append(pc * 100)
 
 print(ar)
-        # plt.xlim([0, 9])
-        # plt.ylim([0, 1])
-        # plt.plot(range(13), M20[i - 9:i + 4], label="M20", linewidth=4, color="g")
-        # plt.plot(range(13), M10[i - 9:i + 4], label="M20", linewidth=4, color="b")
-        # plt.plot(range(13), df["close"][i - 9:i + 4], label="M20", linewidth=4, color="r")
-        # plt.show()
+# plt.xlim([0, 9])
+# plt.ylim([0, 1])
+# plt.plot(range(13), M20[i - 9:i + 4], label="M20", linewidth=4, color="g")
+# plt.plot(range(13), M10[i - 9:i + 4], label="M20", linewidth=4, color="b")
+# plt.plot(range(13), df["close"][i - 9:i + 4], label="M20", linewidth=4, color="r")
+# plt.show()
 
 # plt.plot(df["ind"], M20, label="M20")
 # plt.plot(df["ind"], M10, label="M20")
